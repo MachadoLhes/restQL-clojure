28af6994567e2c3543427c935e65496712fb9668
Iago Oliveira <silva.io@outlook.com>
2017-02-08 15:37:05 -0200

    Adding documentation about hook

--- src/clj/restql/core/hooks/core.clj
+++ src/clj/restql/core/hooks/core.clj
@@ -36,3 +36,19 @@
       (let [hook-fns (hooks hook-name)]
         (doseq [hook-fn hook-fns]
           (hook-fn param-map))))))
+
+(comment
+  "
+  This is the hook map format restQL-core understands, where:
+
+  + hook-type-n => a clojure function
+  + class-type-n => a java Class implementing the hook
+  "
+  hook-format {:clojure-hooks {:before-query [hook-bq-1]
+                               :after-query [hook-aq-1 hook-aq-2]
+                               :before-request [hook-br-1, hook-br-2]
+                               :after-request [hook-ar-1 hook-ar-2 hook-ar-3]}
+               :java-hooks {:before-query [class-bq-1]
+                            :after-query [class-aq-1 class-aq-2]
+                            :before-request [class-br-1, class-br-2]
+                            :after-request [class-ar-1]}} )
