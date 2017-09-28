59bcaef4342c452e37e5580cac8511cfd69c3168
sio-iago <silva.io@outlook.com>
2017-09-28 10:23:50 -0300

    Finishing query parser

--- src/java/restql/core/query/QueryInterpolator.java
+++ src/java/restql/core/query/QueryInterpolator.java
@@ -1,17 +1,47 @@
 package restql.core.query;
 
-public class QueryInterpolator {
+import java.util.List;
+
+public final class QueryInterpolator {
+
+	private QueryInterpolator() {}
+
+	private static <T> String parseParam(T param) {
+		String parsedParam;
+
+		if(param instanceof List) {
+			parsedParam = "[";
+
+			List listParam = (List) param;
+
+			boolean first = true;
+			for(Object listValue : listParam) {
+				if(first) {
+					first = false;
+				}
+				else {
+					parsedParam += ",";
+				}
+				parsedParam += parseParam(listValue);
+			}
+
+			parsedParam += "]";
+		}
+		else if (param instanceof String) {
+			parsedParam = "\"" + param + "\"";
+		} else {
+			parsedParam = param.toString();
+		}
+
+		return parsedParam;
+	}
 
 	public static String interpolate(String query, Object... args) {
 		final String queryWithPlaceHolders = query.replace("?", "%s");
 		final String[] escapedArgs = new String[args.length];
 
 		for (int i = 0; i < args.length; i++) {
-			if (args[i] instanceof String) {
-				escapedArgs[i] = "\"" + args[i] + "\"";
-			} else {
-				escapedArgs[i] = args[i].toString();
-			}
+			escapedArgs[i] = parseParam(args[i]);
 		}
 
 
