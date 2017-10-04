8e83ec5e7cb3a6b79891a9c4dd4ad1b9162d6b6a
Ricardo Mayerhofer <ricardo.mayerhofer@b2wdigital.com>
2017-10-04 14:38:22 -0300

    First version of Lein build.

---
+++ project.clj
@@ -0,0 +1,21 @@
+(defproject restql-core "2.1.0"
+  :description "Microservice query language"
+  :url "https://github.com/B2W-Digital/restQL-core"
+  :license {:name "MIT"
+            :url "http://www.opensource.org/licenses/mit-license.php"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [http-kit "2.2.0"]
+                 [org.clojure/tools.logging "0.3.1"]
+                 [ring/ring-codec "1.0.1"]
+                 [slingshot "0.12.2"]
+                 [instaparse "1.4.8"]
+                 [prismatic/schema "1.1.7"]
+                 [org.clojure/core.async "0.3.443"]
+                 [com.fasterxml.jackson.core/jackson-databind "2.8.5"]
+                 [cheshire "5.5.0"]
+                 [adzerk/bootlaces "0.1.13"]]
+  :source-paths ["src" "src/clj"]
+  :resource-paths ["src/resources"]
+  :test-paths ["test" "test/clj"]
+  :profiles { :uberjar { :aot :all } }
+)
