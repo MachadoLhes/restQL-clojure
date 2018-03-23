71a4c944f1c94f1ed60ed6484f869a469ee39803
Ricardo Mayerhofer <ricardo.mayerhofer@b2wdigital.com>
2018-03-23 18:09:35 -0300

    Adding filter tests.

--- test/integration/restql/filter_test.clj
+++ test/integration/restql/filter_test.clj
@@ -11,13 +11,18 @@
 (defn hero-route []
   {:status 200 :content-type "application/json" :body (json/generate-string hero-response)})
 
+(def heroes-response [ {:id "B20" :name "Batman"} {:id "B30" :name "Superman"} ])
+(defn heroes-route []
+  {:status 200 :content-type "application/json" :body (json/generate-string heroes-response)})
+
 (def sidekick-response {:id "A20" :name "Robin" :heroes [ { :id "B20" :name "Batman" } { :id "B30" :name "Superman" }]})
 (defn sidekick-route []
   {:status 200 :content-type "application/json" :body (json/generate-string sidekick-response)})
 
 (defn execute-query [baseUrl query]
   (restql/execute-query :mappings {:hero                (str baseUrl "/hero")
-                                   :sidekick            (str baseUrl "/sidekick")}
+                                   :sidekick            (str baseUrl "/sidekick")
+                                   :heroes              (str baseUrl "/heroes")}
                         :query query)
 )
 
@@ -53,18 +58,21 @@
 )
 
 (deftest array-multiplex-filter
-  (with-routes! {"/hero" (hero-route) "/sidekick" (sidekick-route)}
-      (let [response (execute-query uri "from hero with id=[1,2]\nfrom sidekick with id = hero.id only heroes.name")
-            first-result (first (get-in response [:sidekick :result]))
-            second-result (second (get-in response [:sidekick :result]))]
+  (with-routes! {"/heroes" (heroes-route)}
+      (let [response (execute-query uri "from heroes with id=[1,2] only name")
+            first-request (first (get-in response [:heroes :result]))
+            first-request-first-hero (first first-request)
 
-        (print (json/generate-string response))
+            second-request (second (get-in response [:heroes :result]))
+            second-request-first-hero (first second-request)]
 
-          (is (= nil (:id (first (:heroes first-result)))))
-          (is (= "Batman" (:name (first (:heroes first-result)))))
+        (pprint response)
 
-          (is (= nil (:id (first (:heroes second-result)))))
-          (is (= "Batman" (:name (first (:heroes second-result)))))
+        (is (= nil (:id first-request-first-hero)))
+        (is (= "Batman" (:name first-request-first-hero)))
+
+        (is (= nil (:id  second-request-first-hero)))
+        (is (= "Superman" (:name first-request-first-hero)))
       )
   )
 )
