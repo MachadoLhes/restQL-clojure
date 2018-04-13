bfa5b4c9bb1183d009c38176bbe09b87d0def861
Ricardo Mayerhofer <ricardo.mayerhofer@b2wdigital.com>
2018-04-13 17:03:46 -0300

    Updating version number.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject b2wdigital/restql-core "2.2"
+(defproject b2wdigital/restql-core "2.2.2"
   :description "Microservice query language"
   :url "https://github.com/B2W-Digital/restQL-core"
   :license {:name "MIT"
