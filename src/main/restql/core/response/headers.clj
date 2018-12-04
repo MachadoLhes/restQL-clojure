882c729fa7641c0959204e1911697958ce6c8605
Rodrigo Machado <souza.machado.rodrigo@gmail.com>
2018-12-04 18:04:49 -0200

    feat(http-header-semantics) make RestQL choose between query cache-control, no-cache and minimal cache-control values

--- src/main/restql/core/response/headers.clj
+++ src/main/restql/core/response/headers.clj
@@ -1,6 +1,7 @@
 (ns restql.core.response.headers
   (:require [clojure.string :as string]
-            [clojure.walk :refer [keywordize-keys]]))
+            [clojure.walk :refer [keywordize-keys]]
+            [clojure.set :refer [rename-keys]]))
 
 (defn map-headers-to-aliases
   "Given a key-value pair, where key is the resource alias
@@ -68,19 +69,23 @@
   )
 )
 
-(defn cache-control-values-to-map [cache-control-values]
-  (->>
-    cache-control-values
-    (map #(string/split % #"="))
+(defn split-cache-control-values [cache-control-values]
+  (map #(string/split % #"=") cache-control-values)
+)
+
+(defn cache-control-values-to-map [splitted-cache-control-values]
+  (->> splitted-cache-control-values
+    (map #(if (= (count %) 1) (conj % true) %))
     (into {})
     (keywordize-keys)
   )
 )
 
-(defn parse-cache-control-values [cache-control-vals]
+(defn parse-cache-control-values [cache-control-values]
   (->>
-    cache-control-vals
+    cache-control-values
     (map #(string/split % #", "))
+    (map split-cache-control-values)
     (map cache-control-values-to-map)
   )
 )
@@ -93,24 +98,67 @@
   )
 )
 
-(defn get-cache-headers
-  "Adds cache control header to header list"
-  [query headers-by-aliases]
-  (->
-    headers-by-aliases
-    (get-cache-control-headers)
+(defn get-minimal-cache-type-value [cache-control-headers cache-type]
+  "Returns a map with the minimal 'max-age' and 's-maxage' values"
+  (->>
+    cache-control-headers
+    (map #(get-in % [cache-type]))
+    (remove nil?)
+    (reduce (fn [p n] (if (compare p n) -1) p n))
+    (assoc {} cache-type)
   )
+)
+
+(defn get-query-cache-control [query]
+  "Returns, if any, the value of query cache-control"
+  (let [query-meta (select-keys (meta query) [:cache-control :max-age :s-maxage])]
+  (if (some? (get query-meta :max-age))
+      (dissoc query-meta :cache-control)
+      (rename-keys query-meta {:cache-control :max-age})
+    )
+  )
+)
+
+(defn get-minimal-response-cache-control-values [cache-control-headers]
+  "Returns a map with minimal 'max-age' and 's-maxage' values"
+  (merge (get-minimal-cache-type-value cache-control-headers :max-age) (get-minimal-cache-type-value cache-control-headers :s-maxage)))
+
+(defn check-query-for-cache-control [query]
+  "Returns true if headers have 'no-cache' or false if it doesn't"
+  (if (empty? (get-query-cache-control query))
+    false
+    true))
+
+(defn check-headers-for-no-cache [cache-control-headers]
+  "Returns true if headers have 'no-cache' or false if it doesn't"
+  (let [no-cache (map #(get-in % [:no-cache]) cache-control-headers)]
+  (if (some true? no-cache)
+    true
+    false))
+)
+
+(defn generate-cache-string [cache-control-headers]
+  "Generates cache string to be associated with :cache-control key"
+  (->> cache-control-headers
+    (map (fn [[a b]] (str (name a) "=" b)))
+    (string/join ", ")))
+
+(defn get-cache-header [query headers-by-aliases]
+  "Adds cache control header to header list"
+  (let [cache-control-headers (get-cache-control-headers headers-by-aliases)]
+  (assoc {} :cache-control 
+    (cond 
+      (check-query-for-cache-control query) (generate-cache-string (get-query-cache-control query))
+      (check-headers-for-no-cache cache-control-headers) "no-cache"
+      :else (generate-cache-string (get-minimal-response-cache-control-values cache-control-headers))
+  )))
 )
 
 (defn get-response-headers [query result]
   (let [headers-by-aliases (map-response-headers-to-aliases result)]
     (->
       (get-alias-suffixed-headers headers-by-aliases)
-      (merge (get-cache-headers query headers-by-aliases))
-
-      ; (get-alias-siffixed-headers result)
-      ; (into (additional-headers interpolated-query))
-      ; (into {"Content-Type" "application/json"})
+      (merge (get-cache-header query headers-by-aliases))
     )
   )
 )
