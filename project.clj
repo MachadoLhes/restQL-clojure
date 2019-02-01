f7b3dcae848db0d735a0aa788f233fd310527530
Lucas Barros <lucsbarros@gmail.com>
2019-02-01 17:44:57 -0200

    bump version to 2.8.6

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject b2wdigital/restql-core "2.8.5"
+(defproject b2wdigital/restql-core "2.8.6"
   :description "Microservice query language"
   :url "https://github.com/B2W-Digital/restQL-clojure"
   :license {:name "MIT"
