8f5b3205c2a37481396f418c908c71115ddbb1cf
Lucas Barros <lucsbarros@gmail.com>
2019-02-11 14:59:22 -0200

    feat(filter): change select to works with nested list [B2W-BIT/restQL-http/issues/81] (#52)

--- src/main/restql/core/transformations/select.clj
+++ src/main/restql/core/transformations/select.clj
@@ -1,17 +1,17 @@
 (ns restql.core.transformations.select
   (:require [restql.core.transformations.filters :as filters]))
 
-(defn select-keyword [select-param entity-value]
+(defn- select-keyword [select-param entity-value]
   (get entity-value select-param))
 
 (declare do-selection)
 
-(defn apply-filters [filter-data entity-value]
+(defn- apply-filters [filter-data entity-value]
   (if (seq filter-data)
     (filters/apply-filters entity-value filter-data)
     entity-value))
 
-(defn select-expression [[select-key & select-params] entity-value]
+(defn- select-expression [[select-key & select-params] entity-value]
   (let [entity-item (get entity-value select-key)
         subselect (->> select-params (filter set?) first)
         filters (filter map? select-params)
@@ -20,12 +20,12 @@
       filtered-items
       (do-selection subselect filtered-items))))
 
-(defn contains-wildcard? [select-params]
+(defn- contains-wildcard? [select-params]
   (if (set? select-params)
     (some #(= :* %) select-params)
     false))
 
-(defn select-single [select-params entity-value]
+(defn- select-single [select-params entity-value]
   (reduce (fn [result select-param]
             (cond
               (= :* select-param) result
@@ -35,27 +35,19 @@
           (if (contains-wildcard? select-params) entity-value {})
           select-params))
 
-(defn do-selection [fields-to-select result]
+(defn- do-selection [fields-to-select result]
   (if (sequential? result)
-    (map #(select-single fields-to-select %) result)
+    (map #(do-selection fields-to-select %) result)
     (select-single fields-to-select result)))
 
+(defn- filter-resource-response [fields-to-select resource-response]
+  (->> resource-response
+       (:result)
+       (do-selection fields-to-select)
+       (assoc {} :result)
+       (merge {:details (:details resource-response)})))
 
-(defn is-multiplexed-request? [resource-response]
-  (sequential? (:details resource-response))
-)
-
-
-(defn filter-resource-response [fields-to-select resource-response]
-  (let [filtered-resource-response {:details (:details resource-response)}]
-    (if (is-multiplexed-request? resource-response)
-      (assoc filtered-resource-response :result (map (partial do-selection fields-to-select) (:result resource-response)))
-      (assoc filtered-resource-response :result (do-selection fields-to-select (:result resource-response)))
-    )
-  )
- )
-
-(defn reduce-with [query]
+(defn- reduce-with [query]
   (fn [acc resource resource-response]
     (let [select-params (:select (resource query))]
       (cond
--- test/integration/restql/filter_test.clj
+++ test/integration/restql/filter_test.clj
@@ -1,76 +1,97 @@
 (ns restql.filter-test
   (:require [clojure.test :refer [deftest is]]
             [restql.core.api.restql :as restql]
+            [restql.test-util :as test-util]
             [cheshire.core :as json]
             [stub-http.core :refer :all]
-            [clojure.pprint :refer :all]
-            )
-)
-
-(def hero-response {:id "B20" :name "Batman" :sidekick "A20" :sidekicks [{ :id "A20" :name "Robin" } { :id "A30" :name "Alfred" } ] })
-(defn hero-route []
-  {:status 200 :content-type "application/json" :body (json/generate-string hero-response)})
-
-(def heroes-response [ {:id "B20" :name "Batman"} {:id "B30" :name "Superman"} ])
-(defn heroes-route []
-  {:status 200 :content-type "application/json" :body (json/generate-string heroes-response)})
-
-(def sidekick-response {:id "A20" :name "Robin" :heroes [ { :id "B20" :name "Batman" } { :id "B30" :name "Superman" }]})
-(defn sidekick-route []
-  {:status 200 :content-type "application/json" :body (json/generate-string sidekick-response)})
+            [clojure.pprint :refer :all]))
 
 (defn execute-query [baseUrl query]
-  (restql/execute-query :mappings {:hero                (str baseUrl "/hero")
-                                   :sidekick            (str baseUrl "/sidekick")
-                                   :heroes              (str baseUrl "/heroes")}
-                        :query query)
-)
+  (restql/execute-query :mappings {:hero     (str baseUrl "/hero")
+                                   :sidekick (str baseUrl "/sidekick")
+                                   :heroes   (str baseUrl "/heroes")}
+                        :query query))
 
 (deftest simple-filter
-    (with-routes! {"/hero" (hero-route)}
-        (let [response (execute-query uri "from hero only name")]
-            (is (= nil (get-in response [:hero :result :id])))
-        )
-    )
-)
+  (with-routes! {(test-util/route-request "/hero")
+                 (test-util/route-response {:id "B20" :name "Batman"})}
+      (let [response (execute-query uri "from hero only name")]
+        (is (= "Batman" (get-in response [:hero :result :name])))
+        (is (= nil      (get-in response [:hero :result :id]))))))
 
 (deftest multiplex-filter
-  (with-routes! {"/hero" (hero-route)}
+  (with-routes! {(test-util/route-request "/hero" {:id 1})
+                 (test-util/route-response {:id "B10" :name "Batman"})
+                 (test-util/route-request "/hero" {:id 2})
+                 (test-util/route-response {:id "B20" :name "Robin"})}
       (let [response (execute-query uri "from hero with id = [1,2] only name")
-              result (get-in response [:hero :result])]
-          (is (= nil (:id (first result))))
-          (is (= nil (:id (second result))))
-      )
-  )
-)
+            result (get-in response [:hero :result])]
+        (is (= nil      (:id (first result))))
+        (is (= "Batman" (:name (first result))))
+        (is (= nil      (:id (second result))))
+        (is (= "Robin"  (:name (second result)))))))
 
 (deftest array-filter
-  (with-routes! {"/hero" (hero-route)}
+  (with-routes! {(test-util/route-request "/hero")
+                 (test-util/route-response {:id "B10" :sidekicks [{:name "Robin"} {:name "Alfred"}]})}
       (let [response (execute-query uri "from hero only sidekicks.name")
             result (get-in response [:hero :result])]
           (is (= nil (:id (first (:sidekicks result)))))
           (is (= "Robin" (:name (first (:sidekicks result)))))
-
           (is (= nil (:id (second (:sidekicks result)))))
-          (is (= "Alfred" (:name (second (:sidekicks result)))))
-        )
-  )
-)
+          (is (= "Alfred" (:name (second (:sidekicks result))))))))
 
 (deftest array-multiplex-filter
-    (with-routes! {"/heroes" (heroes-route)}
-       (let [response (execute-query uri "from heroes with id=[1,2] only name")
-            first-request (first (get-in response [:heroes :result]))
-            first-request-first-hero (first first-request)
+  (with-routes! {(test-util/route-request "/heroes" {:id 1})
+                 (test-util/route-response [{:id "B10" :name "Batman"} {:id "B20" :name "Robin"}])
+                 (test-util/route-request "/heroes" {:id 2})
+                 (test-util/route-response [{:id "B30" :name "Superman"} {:id "B40" :name "Superwoman"}])}
+    (let [response (execute-query uri "from heroes with id=[1,2] only name")
+          first-request (first (get-in response [:heroes :result]))
+          second-request (second (get-in response [:heroes :result]))]
+      (is (= nil      (:id (first first-request))))
+      (is (= "Batman" (:name (first first-request))))
+      (is (= nil      (:id (second first-request))))
+      (is (= "Robin"  (:name (second first-request))))
 
-            second-request (second (get-in response [:heroes :result]))
-            second-request-second-hero (second second-request)]
+      (is (= nil          (:id (first second-request))))
+      (is (= "Superman"   (:name (first second-request))))
+      (is (= nil          (:id (second second-request))))
+      (is (= "Superwoman" (:name (second second-request)))))))
 
-          (is (= nil (:id first-request-first-hero)))
-          (is (= "Batman" (:name first-request-first-hero)))
+(deftest multiplex-with-list
+  (with-routes! {(test-util/route-request "/heroes")
+                 (test-util/route-response [{:id "B10"} {:id "B20"}])
+                 (test-util/route-request "/hero" {:id "B10"})
+                 (test-util/route-response {:id "B10" :name "Batman"})
+                 (test-util/route-request "/hero" {:id "B20"})
+                 (test-util/route-response {:id "B10" :name "Robin"})}
+    (let [response (execute-query uri "from heroes \n from hero with id = heroes.id only name")]
+      (is (= nil      (:id (first (get-in response [:hero :result])))))
+      (is (= "Batman" (:name (first (get-in response [:hero :result])))))
+      (is (= nil      (:id (second (get-in response [:hero :result])))))
+      (is (= "Robin"  (:name (second (get-in response [:hero :result]))))))))
 
-          (is (= nil (:id  second-request-second-hero)))
-          (is (= "Superman" (:name second-request-second-hero)))
-       )
-    )
-)
+(deftest multiplex-with-list
+  (with-routes! {(test-util/route-request "/heroes")
+                 (test-util/route-response {:heroes [{:id "B10"} {:id "B20"}]})
+                 (test-util/route-request "/hero" {:id "B10"})
+                 (test-util/route-response {:id "B10" :name "Batman" :sidekicks [{:name "Catwoman"}]})
+                 (test-util/route-request "/hero" {:id "B20"})
+                 (test-util/route-response {:id "B20" :name "Robin" :sidekicks [{:name "Alfred"}]})
+                 (test-util/route-request "/sidekick" {:name "Catwoman"})
+                 (test-util/route-response {:id "S10" :name "Catwoman" :movie "Batman"})
+                 (test-util/route-request "/sidekick" {:name "Alfred"})
+                 (test-util/route-response {:id "S20" :name "Alfred" :movie "Batman"})}
+    (let [response (execute-query uri "from heroes
+                                         with id = [1,2] \n
+                                       from hero
+                                         with id = heroes.heroes.id
+                                         only name \n
+                                       from sidekick
+                                         with name = hero.sidekicks.name
+                                         only id, name")]
+      (is (= [[{:name "Batman"} {:name "Robin"}]
+              [{:name "Batman"} {:name "Robin"}]] (get-in response [:hero :result])))
+      (is (= [[[{:name "Catwoman" :id "S10"}] [{:name "Alfred" :id "S20"}]]
+              [[{:name "Catwoman" :id "S10"}] [{:name "Alfred" :id "S20"}]]] (get-in response [:sidekick :result]))))))
